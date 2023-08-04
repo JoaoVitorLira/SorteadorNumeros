1dca4361c040c2c0c618a41ef6393c50046769a7
JoaoVitorLira <joao.liraves07@gmail.com>
2023-08-04 15:22:10 -0300

    Initial commit

---
+++ aula.js
@@ -0,0 +1,15 @@
+//Math.random() gera números aleatótios
+console.log(Math.random())
+console.log(Math.random() * 20)
+console.log(Math.random(7 - 3 + 1)+3)
+
+//Arredonda para o valor inteiro mais próximo
+console.log(Math.round(3.1))
+
+//Arredonda pra baixo
+console.log(Math.floor(3.1))
+console.log(Math.floor(3.9))
+
+//Arredonda pra cima
+console.log(Math.ceil(3.1))
+console.log(Math.ceil(3.9))
