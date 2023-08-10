be8d4c5deb2f062a5c4cb0aa29b639b3110ae728
JoaoVitorLira <joao.liraves07@gmail.com>
2023-08-10 15:51:54 -0300

    Adição de menu e responsividade

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ button.addEventListener('click', function(){
 
     document.querySelector('#resultado').textContent = resultado
 })
+
+
+function mostrarMenu() {
+    var menu = document.getElementById("nav-header")
+    var icone = document.getElementById("icone")
+
+    if (getComputedStyle(menu).display == 'none') {
+        menu.style.display = "flex";
+        icone.classList.remove("fa-bars")
+        icone.classList.add("fa-times")
+    } else {
+        menu.style.display = "none";
+        icone.classList.remove("fa-times");
+        icone.classList.add("fa-bars");
+    }
+}
